cafacbeb8084b3aa7dec23254052b1a53c7af3f3
Guilherme Silva Fonseca <49875835+guisfons@users.noreply.github.com>
2024-10-10 15:24:23 -0300

    Several changes to make it work

--- script.js
+++ script.js
@@ -1,72 +1,97 @@
 document.addEventListener('DOMContentLoaded', function () {
+  function getUrlParams() {
+    const params = new URLSearchParams(window.location.search);
+    let paramsObject = {};
+
+    if (params.toString()) {
+        for (const [key, value] of params.entries()) {
+            paramsObject[key] = value;
+        }
+        return paramsObject;
+    }
+
+    const storedParams = localStorage.getItem('urlParams');
+    if (storedParams) {
+        const parsedParams = JSON.parse(storedParams);
+        
+        // Check if the item has expired
+        const now = new Date();
+        if (parsedParams.expiry && now.getTime() > parsedParams.expiry) {
+            console.log('Stored URL parameters have expired');
+            return paramsObject;  // Return empty object since params are expired
+        }
+
+        return parsedParams.value;
+    }
+
+    return paramsObject;
+  }
+
   function setLocalStorageWithExpiry(key, value, ttl) {
-    var now = new Date()
-    var item = {
-      value: value,
-      expiry: now.getTime() + ttl
-    }
-    localStorage.setItem(key, JSON.stringify(item))
+      const now = new Date();
+      const item = {
+          value: value,
+          expiry: now.getTime() + ttl
+      };
+      localStorage.setItem(key, JSON.stringify(item));
   }
 
-  function getLocalStorageWithExpiry(key) {
-    var itemStr = localStorage.getItem(key)
-    if (!itemStr) {
-      return null
-    }
-    var item = JSON.parse(itemStr)
-    var now = new Date()
-    if (now.getTime() > item.expiry) {
-      localStorage.removeItem(key)
-      return null
-    }
-    return item.value
+  function saveParamsToLocalStorageWithExpiry(ttl) {
+      const urlParams = getUrlParams();
+      setLocalStorageWithExpiry('urlParams', urlParams, ttl);
   }
 
-  function storeAllUrlParams() {
-    var urlParams = new URLSearchParams(window.location.search)
-    var ttl = 45 * 24 * 60 * 60 * 1000 // 45 days in milliseconds
+  var ttl = 45 * 24 * 60 * 60 * 1000 // 45 dias
+  saveParamsToLocalStorageWithExpiry(ttl);
 
-    urlParams.forEach(function (value, key) {
-      var storedValue = getLocalStorageWithExpiry(key)
-      if (storedValue !== value) {
-        setLocalStorageWithExpiry(key, value, ttl)
-      }
-    })
-  }
+  function addLocalStorageParamsToUrl() {
+    const storedParams = localStorage.getItem('urlParams');
+    
+    if (storedParams) {
+        const parsedParams = JSON.parse(storedParams);
+        
+        const now = new Date();
+        if (parsedParams.expiry && now.getTime() > parsedParams.expiry) {
+            console.log('Stored URL parameters have expired');
+            return;
+        }
 
-  storeAllUrlParams()
-
-  function getAllStoredParams() {
-    var url = ''
-    for (var i = 0; i < localStorage.length; i++) {
-      var key = localStorage.key(i)
-      var value = getLocalStorageWithExpiry(key)
-      if (value) {
-        url += key + '=' + value + '&'
-      }
+        const urlParams = new URLSearchParams(parsedParams.value);
+        // Append the parameters to the current URL
+        // const newUrl = `${window.location.pathname}?${urlParams.toString()}`;
+        // window.history.replaceState(null, '', newUrl);
+        return urlParams.toString()
+    } else {
+        console.log('No URL parameters found in localStorage');
     }
-    return url.slice(0, -1) // Remove the trailing '&'
   }
+
+  addLocalStorageParamsToUrl();
 
   function changeFormAction() {
     var cubilisReservationForms = document.querySelectorAll('form[action^="https://reservations.cubilis.eu"]')
     var cubilisReservationLinks = document.querySelectorAll('a[href^="https://reservations.cubilis.eu"]')
     var hoteliersReservationLinks = document.querySelectorAll('a[href^="https://ibe.hoteliers.guru"]')
     var hoteliersReservationForms = document.querySelectorAll('form[action^="https://ibe.hoteliers.guru"]')
-    var url = getAllStoredParams()
+    var url = '?'
+
+    if (localStorage.getItem('urlParams')) {
+      url += addLocalStorageParamsToUrl()
+    }
 
     // Cubilis
     if (cubilisReservationForms.length > 0) {
       Array.from(cubilisReservationForms).forEach(function (reservationForm) {
         reservationForm.querySelector('#startdate').setAttribute('required', 'required')
         reservationForm.querySelector('#enddate').setAttribute('required', 'required')
-        reservationForm.action += '?' + url
+
+        reservationForm.action += url
       })
     }
 
     if (cubilisReservationLinks.length > 0) {
       Array.from(cubilisReservationLinks).forEach(function (reservationLink) {
-        reservationLink.href += '?' + url
+        reservationLink.href += url
       })
     }
 
@@ -76,34 +101,32 @@ document.addEventListener('DOMContentLoaded', function () {
 
       d21Trigger.forEach(function (trigger) {
         trigger.addEventListener('click', function () {
-          setTimeout(function () {
-            let dirsIframeContainer = document.querySelector('#d21-modal-fullsize')
-
-            if (dirsIframeContainer) {
-              let dirsIframe = dirsIframeContainer.querySelector('iframe')
-
-              if (dirsIframe) {
-                let separator = dirsIframe.src.includes('?') ? '&' : '?'
-
-                dirsIframe.addEventListener('load', function () {
-                  this.src += separator + getAllStoredParams()
-                }, { once: true })
-              }
-            }
-          }, 2000)
-        })
+			setTimeout(function () {
+				let dirsIframeContainer = document.querySelector('#d21-modal-fullsize')
+				if (dirsIframeContainer) {
+					let dirsIframe = dirsIframeContainer.querySelector('iframe')
+					console.log('dirsIframe: ' + dirsIframe)
+					if (dirsIframe) {
+						dirsIframe.addEventListener('load', function () {
+							this.src += url
+						}, { once: true })
+					}
+				}
+			}, 2000)
+		})
       })
     }
 
+    // Hoteliers Guru
     if (hoteliersReservationLinks.length > 0) {
       Array.from(hoteliersReservationLinks).forEach(function (hoteliersReservationLink) {
-        hoteliersReservationLink.href += '?' + url
+        hoteliersReservationLink.href += url
       })
     }
 
     if (hoteliersReservationForms.length > 0) {
       Array.from(hoteliersReservationForms).forEach(function (reservationForm) {
-        reservationForm.action += '?' + url
+        reservationForm.action += url
       })
     }
   }
